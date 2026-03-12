52571aa21b71948c5b8344799c5e660b90f1385c
studigiano <meiermichael@outlook.de>
2026-03-12 12:47:54 +0100

    Create check.py

---
+++ check.py
@@ -0,0 +1,28 @@
+import requests
+import os
+
+DOMAINS = [
+    "meinedomain.de",
+    "meinedomain.com"
+]
+
+TOKEN = os.environ["TELEGRAM_TOKEN"]
+CHAT_ID = os.environ["TELEGRAM_CHAT_ID"]
+
+
+def send(msg):
+    url = f"https://api.telegram.org/bot{TOKEN}/sendMessage"
+    requests.post(url, data={"chat_id": CHAT_ID, "text": msg})
+
+
+def check(domain):
+    url = "https://api.domainsdb.info/v1/domains/search"
+    r = requests.get(url, params={"domain": domain.split(".")[0]})
+    data = r.json()
+
+    if "domains" not in data:
+        send(f"🔥 DOMAIN FREI: {domain}")
+
+
+for d in DOMAINS:
+    check(d)
